d21012a5a1f461510104dfb073b329f83dbc7c9a
Lais Costa <lcosta.engqua@gmail.com>
2025-09-06 21:33:10 -0300

    ajustes

--- db/create-db-template.sql
+++ db/create-db-template.sql
@@ -2,8 +2,6 @@ CREATE DATABASE boletim
     DEFAULT CHARACTER SET = 'utf8mb4';
 
     USE boletim;
-
-    -- DROP TABLE usuarios;
     CREATE TABLE usuarios(
       id INT PRIMARY KEY AUTO_INCREMENT,
       nome VARCHAR(100) NOT NULL,
@@ -21,12 +19,6 @@ CREATE DATABASE boletim
       data_criacao DATETIME DEFAULT CURRENT_TIMESTAMP,
       data_atualizacao DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
     );
-
-    INSERT INTO usuarios VALUES(1, 'aluno1', '00000000000', '1980-09-01', 1, 'aluno1@teste', '75000000000', 'Rua A', '1', 'Centro', 'Fsa', 'Bahia', 'Brasil', null, null);
-    INSERT INTO usuarios VALUES(2, 'aluno2', '00000000000', '1980-09-01', 2, 'aluno2@teste', '75000000000', 'Rua A', '1', 'Centro', 'Fsa', 'Bahia', 'Brasil', null, null);
-    INSERT INTO usuarios VALUES(3, 'aluno3', '00000000000', '1980-09-01', 3, 'aluno3@teste', '75000000000', 'Rua A', '1', 'Centro', 'Fsa', 'Bahia', 'Brasil', null, null);
-    INSERT INTO usuarios VALUES(4, 'aluno4', '00000000000', '1980-09-01', 4, 'aluno4@teste', '75000000000', 'Rua A', '1', 'Centro', 'Fsa', 'Bahia', 'Brasil', null, null);
-    -- DELETE FROM usuarios;
     SELECT * FROM usuarios;
 
     CREATE TABLE cursos(
@@ -37,11 +29,6 @@ CREATE DATABASE boletim
       data_criacao DATETIME DEFAULT CURRENT_TIMESTAMP,
       data_atualizacao DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
     );
-
-    INSERT INTO cursos VALUES(1, 'Java', 'Destinado ao ensinamento de Programação em Java', 1, null, null);
-    INSERT INTO cursos VALUES(2, 'Dados', 'Destinado ao ensinamento de Análise de Dados', 1, null, null);
-    INSERT INTO cursos VALUES(3, 'PHP', 'Destinado ao ensinamento de Programação em PHP', 1, null, null);
-    UPDATE cursos SET ativo = 'N' WHERE id = 1;
     SELECT * FROM cursos;
 
     -- 1:N Um curso pode ter várias turmas
