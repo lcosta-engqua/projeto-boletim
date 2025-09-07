0bf904673a00ad4ffc8bcffb55630a3180f7e43c
Lais Costa <lcosta.engqua@gmail.com>
2025-09-06 21:29:49 -0300

    Add

--- db/create-db-template.sql
+++ db/create-db-template.sql
@@ -21,12 +21,6 @@ CREATE DATABASE boletim
       data_criacao DATETIME DEFAULT CURRENT_TIMESTAMP,
       data_atualizacao DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
     );
-
-    INSERT INTO usuarios VALUES(1, 'aluno1', '00000000000', '1980-09-01', 1, 'aluno1@teste', '75000000000', 'Rua A', '1', 'Centro', 'Fsa', 'Bahia', 'Brasil', null, null);
-    INSERT INTO usuarios VALUES(2, 'aluno2', '00000000000', '1980-09-01', 2, 'aluno2@teste', '75000000000', 'Rua A', '1', 'Centro', 'Fsa', 'Bahia', 'Brasil', null, null);
-    INSERT INTO usuarios VALUES(3, 'aluno3', '00000000000', '1980-09-01', 3, 'aluno3@teste', '75000000000', 'Rua A', '1', 'Centro', 'Fsa', 'Bahia', 'Brasil', null, null);
-    INSERT INTO usuarios VALUES(4, 'aluno4', '00000000000', '1980-09-01', 4, 'aluno4@teste', '75000000000', 'Rua A', '1', 'Centro', 'Fsa', 'Bahia', 'Brasil', null, null);
-    -- DELETE FROM usuarios;
     SELECT * FROM usuarios;
 
     CREATE TABLE cursos(
@@ -37,9 +31,5 @@ CREATE DATABASE boletim
       data_criacao DATETIME DEFAULT CURRENT_TIMESTAMP,
       data_atualizacao DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
     );
-
-    INSERT INTO cursos VALUES(1, 'Java', 'Destinado ao ensinamento de Programação em Java', 1, null, null);
-    INSERT INTO cursos VALUES(2, 'Dados', 'Destinado ao ensinamento de Análise de Dados', 1, null, null);
-    INSERT INTO cursos VALUES(3, 'PHP', 'Destinado ao ensinamento de Programação em PHP', 1, null, null);
 
     SELECT * FROM cursos;
